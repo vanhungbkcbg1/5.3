2f8e7c74f7ae54ec2ce02411e7c0b57a9d47e155
Mr.Steven <vanhungbkcbg1@gmail.com>
2017-04-28 07:41:46 +0700

    call wcf service

---
+++ app/Jobs/SendReminderEmail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\OrderShipped;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
+
+class SendReminderEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+        Mail::to('vanhungbkcbg1@gmail.com')->send(new OrderShipped());
+    }
+}
