7811bfb655b4bb28ebdd1d1efdd1577eb591e3cf
Mr.Steven <vanhungbkcbg1@gmail.com>
2017-05-18 13:37:11 +0700

    chat template

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,18 +3,27 @@
  */
 
 //window.Pusher = require('pusher-js');
-Echo.channel('test')
-    .listen('ChatMessageWasReceived', function (e) {
-        console.log(e);
-    });
+var socketId = Echo.socketId();
+console.log(socketId);
+//Echo.channel('test')
+//    .listen('ChatMessageWasReceived', function (e) {
+//        console.log(e);
+//    });
 
-Echo.private("chat").listen('Chat', function (e) {
+var pusher=Echo.private("chat").listen('Chat', function (e) {
 
     var new_message=$("#content_right").clone();
     new_message.find('.direct-chat-text').text(e.message);
     $("#direct-chat-messages").append(new_message);
 
     $("#direct-chat-messages").animate({ scrollTop: $('#direct-chat-messages').prop("scrollHeight")}, 200);
+});
+
+pusher.on('pusher:subscription_succeeded', function () {
+
+    //get socket id
+    var socket_id = Echo.socketId();
+    $.ajaxSetup({ headers: {'X-Socket-ID': socket_id} });
 });
 
 
