e7bf0439d6395eb26d02cc67382660e24254c3fc
Mr.Steven <vanhungbkcbg1@gmail.com>
2017-09-25 10:36:58 +0700

    chat template

--- app/Mail/OrderShipped.php
+++ app/Mail/OrderShipped.php
@@ -33,7 +33,7 @@ class OrderShipped extends Mailable
 //        $file = fopen('wfio://'.'過去問.pdf', 'rb');
         return $this->view('emails.test')
             //pass param to view
-//            ->with(['name'=>'vanhung','imagePath'=>public_path('embed.jpg')])
+            ->with(['name'=>'vanhung','imagePath'=>public_path('embed.jpg')])
 //            ->attachData(file_get_contents('wfio://'.'過去問.pdf'),'過去問.pdf',['mime' => 'application/pdf'])
             //attach file
 //            ->attach(public_path('laravel authentication.docx'))
