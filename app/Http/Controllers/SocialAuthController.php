e85c553a143b48b04a438fe97458a43c3bd6960f
hungnv <vanhungbkcbg1@gmail.com>
2018-03-31 08:35:15 +0700

    ftp file upload

---
+++ app/Http/Controllers/SocialAuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Socialite;
+
+class SocialAuthController extends Controller
+{
+    //
+
+    public function redirect()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    public function callback()
+    {
+        // when facebook call us a with token
+    }
+}
---
+++ resources/views/lte/auto.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.lte')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-6">
+            <input type="text" placeholder="type to search" class="form-control" id="auto-completed">
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script src="{{asset('js/lte/index.js')}}"></script>
+@endsection
