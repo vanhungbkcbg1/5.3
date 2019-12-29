dec72567aadb28178cb4df4d6ac3619d1da5a894
hungnv <vanhungbkcbg1@gmail.com>
2019-12-29 23:05:48 +0700

    test du

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -5,4 +5,10 @@ class DemoController extends Controller
     public function test(){
         return "helloword";
     }
+
+    public function test(){
+        if(1)
+        return 1;
+        else return 3;
+    }
 }
