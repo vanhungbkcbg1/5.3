18c1c64ea37798fbe4bb8e9153528838f41642bb
hungnv <vanhungbkcbg1@gmail.com>
2020-01-05 11:36:20 +0700

    d

--- app/lib/Test.php
+++ app/lib/Test.php
@@ -9,5 +9,21 @@ class Test
     public function doSomething(){
         return 'do some thing';
     }
+    public function helloWorld()
+    {
+        return "hello world";
+    }
 
+    public function hi(){
+        return 'hi';
+    }
+
+
+    public function hiHung(){
+        return 'hi Hung';
+    }
+
+    public function helloHung(){
+        return "hello Hung";
+    }
 }
