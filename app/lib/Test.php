f5e0ca866735089e19ef13a451fbbe1566863cfd
hungnv <vanhungbkcbg1@gmail.com>
2020-01-05 10:54:56 +0700

    add

---
+++ app/lib/Test.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\lib;
+class Test
+{
+    public function test(){
+        return "test"
+    }
+
+    public function doSomething(){
+        return 'do some thing';
+    }
+
+}
